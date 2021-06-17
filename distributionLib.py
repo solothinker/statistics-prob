66bdc4b17d6dc19e1d8eb4c9a7cdf4d27b600254
Ashutosh Bhargava <ashutumho@gmail.com>
2021-06-17 23:37:15 +0530

    Z table

--- distributionLib.py
+++ distributionLib.py
@@ -1,16 +1,24 @@
 # A library for understanding the different Probability distribution
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from tabulate import tabulate
+
 plt.rcParams["figure.figsize"] = (20,10)
 np.random.seed(404)
 
 class Dist:
+    def __init__(self):
+        pass
 
     def MOE(rv,moe=0.95,plot=False):
+        #Margin of Error
+        
         pass
         
-    def normalDist(xAxis=np.arange(-1,1.01,0.01),xBar=0,sd=1,plot=False):
+    def normalDist(self,xAxis=np.arange(-1,1.01,0.01),xBar=0,sd=1,plot=False):
         normalDist = 1/(sd*(2*np.pi)**0.5)*np.exp(-((xAxis-xBar)/sd)**2*1/2)
         if plot:
             plt.plot(xAxis,normalDist,label='Normal Distribution')
@@ -20,5 +28,30 @@ class Dist:
             plt.title('Normal Distribution')
             plt.grid()
             plt.show()
-        return xAxis,normalDist
+        return normalDist
+
+    def __str__(self):
+        return f"Distribution object"
+
+
+    
+    def zTable(x=0):
+
+        def nCDF(x=0):
+        # normal Cumulative Distribution Function
+        # yup, I can use the normalDist object I am making it just for fun
+            return 1/(np.sqrt(2*np.pi))*np.exp(-np.square(x)/2)
+        if x:
+            res, err = quad(nCDF,np.Inf, x)
+            return np.round(res,5)
+        else:
+            columns=np.arange(0.00,0.1,0.01)
+            index=np.round(np.arange(0.00,4.1,0.1),3)
+            df = pd.DataFrame(columns=columns,index=index)
+            for ii in index:
+                for jj in columns:
+                    df.loc[ii,jj],_ = np.round(np.abs(quad(nCDF,np.Inf, ii+jj)),5)
+            print(tabulate(df, headers = 'keys', tablefmt = 'pretty'))
+
+            
         
--- testingDist.py
+++ testingDist.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = (20,10)
+from distributionLib import Dist
 np.random.seed(404)
 
 sample = 100
@@ -8,27 +9,24 @@ bins = 4
 
 data = np.random.choice([0, 1], size=(sample,), p=[0.57, 0.43])
 
-suces = float(np.sum(data)/len(data))
-fail = float((len(data)-np.sum(data))/len(data))
+suces = np.sum(data) 
+fail = len(data)-np.sum(data)
 
-xBar = 0*fail+1*suces
-var = fail*(0-xBar)**2+suces*(1-xBar)**2
+xBar = (0*fail+1*suces)/len(data)
+var = (fail*(0-xBar)**2+suces*(1-xBar)**2)/(len(data)-1)
 sd = var**0.5
 
 xDist = xBar*10*sd
 xAxis = np.arange(-xDist,xDist,0.01)+xBar
-
-normalDist = 1/(sd*(2*np.pi)**0.5)*np.exp(-((xAxis-xBar)/sd)**2*1/2)
 yoX = np.arange(-sd*3,sd*4,sd)+xBar
-normalDist2 = 1/(sd*(2*np.pi)**0.5)*np.exp(-((yoX-xBar)/sd)**2*1/2)
+sdOfSamp = sd/np.sqrt(sd)
+print(sd,sdOfSamp)
+normalDist = Dist.normalDist(xAxis,xBar,sd)
+normalDist2 = Dist.normalDist(yoX,xBar,sd)
 
 ##plt.vlines(yoX,normalDist2*0,normalDist2,colors='teal')
-plt.plot(xAxis,normalDist)
-plt.scatter(yoX,normalDist2)
-
-[plt.axvline(ii,ls='--') for ii in yoX]
-plt.show()
-
-##plt.hist(data,bins=bins)
-##plt.xticks([0.125,0.875],["Fail","Success"])
+##plt.plot(xAxis,normalDist)
+##plt.scatter(yoX,normalDist2)
+##
+##[plt.axvline(ii,ls='--') for ii in yoX]
 ##plt.show()
